2a1b655c4a38e39f28f406067059b3f766072a28
jodhan-techinject <jodhansaji@gmail.com>
2024-11-15 15:28:45 +0530

    dark map added for 3d map

--- src/lib/map-libre/components/MapLibreWrapper.svelte
+++ src/lib/map-libre/components/MapLibreWrapper.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import {MapLibre} from "svelte-maplibre";
+    import {darkMode} from "$lib/common/utils/store";
+
     const mapTilerApiKey = import.meta.env.VITE_MAPTILER_API;
 
     export let center: [number, number] = [76.4711, 9.8505]
@@ -8,11 +10,12 @@
     export let pitch: number = 0
     export let darkMapStyle: boolean = true
 
+
 </script>
 
 <MapLibre
         pitch={pitch}
-        style={darkMapStyle ? 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json' : `https://api.maptiler.com/maps/streets/style.json?key=${mapTilerApiKey}`}
+        style={darkMapStyle ? 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json' : `https://api.maptiler.com/maps/${$darkMode ? 'streets-dark' : 'streets'}/style.json?key=${mapTilerApiKey}`}
         class={mapClasses}
         standardControls
         zoom={zoom}

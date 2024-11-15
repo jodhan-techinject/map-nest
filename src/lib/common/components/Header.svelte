f7e8051b06f945e1d7aef4d144ec0f2496593893
jodhan-techinject <jodhansaji@gmail.com>
2024-11-15 11:12:20 +0530

    darkmode added in header

--- src/lib/common/components/Header.svelte
+++ src/lib/common/components/Header.svelte
@@ -2,12 +2,12 @@
     import {darkMode} from "$lib/common/utils/store";
 </script>
 
-<header class=" shadow-sm shadow-primary-200 bg-black">
+<header class=" shadow-sm shadow-primary-200  {$darkMode ? 'bg-black' : ' bg-primary-100'}">
     <div class="container mx-auto px-4 py-3 flex items-center justify-between">
         <div class="flex items-center space-x-4 group cursor-pointer">
             <a href="/" class="p-2 rounded-full hover:bg-primary-foreground/10" aria-label="Menu">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fcfcfc" viewBox="0 0 256 256"
-                     class=" group-hover:fill-primary-200 transition-all">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="{$darkMode ? '#EEEEEE' : '#151515'}" viewBox="0 0 256 256"
+                     class=" group-hover:fill-primary-200 transition-all ">
                     <path d="M228.92,49.69a8,8,0,0,0-6.86-1.45L160.93,63.52,99.58,32.84a8,8,0,0,0-5.52-.6l-64,16A8,8,0,0,0,24,56V200a8,8,0,0,0,9.94,7.76l61.13-15.28,61.35,30.68A8.15,8.15,0,0,0,160,224a8,8,0,0,0,1.94-.24l64-16A8,8,0,0,0,232,200V56A8,8,0,0,0,228.92,49.69ZM104,52.94l48,24V203.06l-48-24ZM40,62.25l48-12v127.5l-48,12Zm176,131.5-48,12V78.25l48-12Z"></path>
                 </svg>
             </a>
@@ -16,7 +16,7 @@
         <nav class="flex gap-7">
             {#if $darkMode}
                 <button on:click={()=>{darkMode.update(()=>false)}} target="_blank" aria-label="github-link">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#f5eaea" viewBox="0 0 256 256" class="hover:fill-primary-200 transition-all">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#f5eaea" viewBox="0 0 256 256" class="hover:fill-primary-200 transition-all ">
                         <path d="M233.54,142.23a8,8,0,0,0-8-2,88.08,88.08,0,0,1-109.8-109.8,8,8,0,0,0-10-10,104.84,104.84,0,0,0-52.91,37A104,104,0,0,0,136,224a103.09,103.09,0,0,0,62.52-20.88,104.84,104.84,0,0,0,37-52.91A8,8,0,0,0,233.54,142.23ZM188.9,190.34A88,88,0,0,1,65.66,67.11a89,89,0,0,1,31.4-26A106,106,0,0,0,96,56,104.11,104.11,0,0,0,200,160a106,106,0,0,0,14.92-1.06A89,89,0,0,1,188.9,190.34Z"></path>
                     </svg>
                 </button>
@@ -29,7 +29,7 @@
             {/if}
 
             <a href='https://github.com/jodhan-techinject' target="_blank" aria-label="github-link">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fcfcfc" viewBox="0 0 256 256"
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="{$darkMode ? '#EEEEEE' : '#151515'}" viewBox="0 0 256 256"
                      class=" hover:fill-primary-200 transition-all">
                     <path d="M208.31,75.68A59.78,59.78,0,0,0,202.93,28,8,8,0,0,0,196,24a59.75,59.75,0,0,0-48,24H124A59.75,59.75,0,0,0,76,24a8,8,0,0,0-6.93,4,59.78,59.78,0,0,0-5.38,47.68A58.14,58.14,0,0,0,56,104v8a56.06,56.06,0,0,0,48.44,55.47A39.8,39.8,0,0,0,96,192v8H72a24,24,0,0,1-24-24A40,40,0,0,0,8,136a8,8,0,0,0,0,16,24,24,0,0,1,24,24,40,40,0,0,0,40,40H96v16a8,8,0,0,0,16,0V192a24,24,0,0,1,48,0v40a8,8,0,0,0,16,0V192a39.8,39.8,0,0,0-8.44-24.53A56.06,56.06,0,0,0,216,112v-8A58.14,58.14,0,0,0,208.31,75.68ZM200,112a40,40,0,0,1-40,40H112a40,40,0,0,1-40-40v-8a41.74,41.74,0,0,1,6.9-22.48A8,8,0,0,0,80,73.83a43.81,43.81,0,0,1,.79-33.58,43.88,43.88,0,0,1,32.32,20.06A8,8,0,0,0,119.82,64h32.35a8,8,0,0,0,6.74-3.69,43.87,43.87,0,0,1,32.32-20.06A43.81,43.81,0,0,1,192,73.83a8.09,8.09,0,0,0,1,7.65A41.72,41.72,0,0,1,200,104Z"></path>
                 </svg>

5228001257a4be2feef8de10ba0c42b957581cf5
jodhan-techinject <jodhansaji@gmail.com>
2024-11-15 12:19:18 +0530

    home dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ export default {
                     300: '#A91D3A',
                     200: '#C73659',
                     100: '#EEEEEE',
+                    50:'#B7B7B7'
                 },
             }
         }

147bdb6b8859e0dfec94365d807396c5b6f55df2
jodhan-techinject <jodhansaji@gmail.com>
2024-11-15 14:30:22 +0530

    fixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,12 @@
     import "../app.css";
     import Header from "$lib/common/components/Header.svelte";
     import { darkMode } from "$lib/common/utils/store";
+    import {onMount} from "svelte";
+    import {initializeDarkMode} from "$lib/common/utils/localStorageController";
+
+    onMount(()=>{
+        initializeDarkMode()
+    })
 </script>
 
 
